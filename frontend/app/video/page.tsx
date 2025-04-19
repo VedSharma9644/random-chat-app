bc8860048ec0f2539569dbc9b23b588e27ec1ddd
VedSharma9644 <58023735+VedSharma9644@users.noreply.github.com>
2025-04-19 23:30:14 +0530

    fixed video call v -- 3

--- frontend/app/video/page.tsx
+++ frontend/app/video/page.tsx
@@ -94,7 +94,7 @@ const Page = () => {
 
       <button
         onClick={startCall}
-        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 "
       >
         Start Call
       </button>
